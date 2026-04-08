bbcbc6fc766d54bf7c289a4b630e92754b56a193
Gerson Soto <marconiimpara00692@gmail.com>
2026-04-08 16:08:59 -0500

    Fix base path para Netlify: usar './'

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/la-perrada-pos/' : '/',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
